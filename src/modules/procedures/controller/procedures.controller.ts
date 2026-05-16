adea018e692ce8533637830eb3d0fec854e92477
Tulio Perdigão <116309232+tulioperdigao@users.noreply.github.com>
2026-05-16 14:21:29 -0300

    feat: implement Procedures module with CRUD operations and add EditorConfig

--- src/modules/procedures/controller/procedures.controller.ts
+++ src/modules/procedures/controller/procedures.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import { ProceduresService } from '../services'
 
-@Controller('procedures')
-export class ProceduresController {}
+@Controller({
+  version: '1',
+  path: 'procedures',
+})
+export class ProceduresController {
+  constructor(private readonly proceduresService: ProceduresService) {}
+
+  @Get()
+  findAll() {
+    return this.proceduresService.findAll()
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.proceduresService.findById(id)
+  }
+
+  @Post()
+  create(@Body() data: any) {
+    return this.proceduresService.create(data)
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() data: any) {
+    return this.proceduresService.update(id, data)
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.proceduresService.delete(id)
+  }
+}
--- src/modules/procedures/services/procedures.service.ts
+++ src/modules/procedures/services/procedures.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class ProceduresService {}
+export class ProceduresService {
+  findAll() {
+    return ['teste1', 'teste2']
+  }
+
+  findById(id: string) {
+    return `teste com id ${id} como Param`
+  }
+
+  create(data: any) {
+    return `teste com ${data} no Body`
+  }
+
+  update(id: string, data: any) {
+    return `teste com id ${id} como Param e com ${data} no Body`
+  }
+
+  delete(id: string) {
+    return `teste rota delete com id ${id} como Param`
+  }
+}
